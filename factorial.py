53eb97df95ff9db9872486e0ec0e328d17ff266f
swethakdbl <ammuk7092@gmail.com>
2024-09-27 14:23:05 +0530

    Advanced sub array problem.py

--- factorial.py
+++
@@ -1,8 +0,0 @@
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * factorial(n - 1)
-number = int(input("Enter a number: "))
-result = factorial(number)
-print(f"Factorial of {number} is: {result}")
